e3e10ca6ac256708b83c09f4e3cfd82117d2bb47
bitnykk <beatnykk@gmx.fr>
2025-12-18 16:32:03 +0100

    Making Test able to list per type and export specific Mesh by ID

--- Test/Program.cs
+++ Test/Program.cs
@@ -25,71 +25,92 @@ namespace Test
 
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length < 1)
             {
-                Console.WriteLine("Command use : Test \"X:Your\\Ao\\Path\" [optional:Type]");
+                Console.WriteLine("Usage : Test \"X:\\AO\\Path\" [Type] [Optional:ID]");
+                return;
             }
-            else
+
+            string aoPath = args[0];
+            Directory.SetCurrentDirectory(aoPath);
+
+            _rdbController = new RdbController(aoPath);
+            var info = _rdbController.Get<InfoObject>(1);
+            var types = info.Types;
+
+            // --- 1: pure list ---
+            if (args.Length == 1)
             {
-                int ICount = 0;
-                string aoPath = args[0];
-                Directory.SetCurrentDirectory(aoPath);
-                _rdbController = new RdbController(aoPath);
-                var types = _rdbController.Get<InfoObject>(1).Types;
-                if (args.Length == 1)
+                Console.WriteLine("Available TYPES :");
+                foreach (var type in types)
+                    Console.WriteLine(type.Key);
+                goto END;
+            }
+
+            string catType = args[1];
+            ResourceTypeId resourceType;
+
+            switch (catType)
+            {
+                case "RdbMesh":
+                    resourceType = ResourceTypeId.RdbMesh;
+                    break;
+                case "CatMesh":
+                    resourceType = ResourceTypeId.CatMesh;
+                    break;
+                case "Anim":
+                    resourceType = ResourceTypeId.Anim;
+                    break;
+                case "Texture":
+                    resourceType = ResourceTypeId.Texture;
+                    break;
+                default:
+                    Console.WriteLine("Unknown type.");
+                    goto END;
+            }
+
+            // --- 2 : extraction ABIFF by ID ---
+            if (args.Length >= 3)
+            {
+                int id = int.Parse(args[2]);
+
+                Console.WriteLine($"Extracting ABIFF from RdbMesh ID={id}");
+
+                Directory.CreateDirectory("export");
+
+                // official API
+                RDBMesh catMesh = _rdbController.Get<RDBMesh>(id);
+
+                if (catMesh == null)
                 {
-                    Console.WriteLine("Available TYPES :");                    
-                    foreach (var type in types)
-                    {
-                        ICount = ICount + 1;
-                        Console.WriteLine(type);
-                    }
+                    Console.WriteLine("Mesh not found.");
+                    goto END;
                 }
+
+                string outPath = Path.Combine("export", $"{id}.abiff");
+
+                // official rewrite
+                byte[] abiffData = catMesh.Serialize();
+                File.WriteAllBytes(outPath, abiffData);
+
+                Console.WriteLine($"ABIFF exported to {outPath}");
+                goto END;
+            }
+
+            // --- 3 : list all IDs ---
+            Console.WriteLine($"Stored {catType} :");
+            foreach (var key in _rdbController.RecordTypeToId[(int)resourceType].Keys)
+            {
+                if (!types[resourceType].TryGetValue(key, out string name))
+                    Console.WriteLine($"{key} : Unnamed");
                 else
-                {
-                    string CatType = args[1];
-                    var CatList = new ResourceTypeId[0];
-                    switch (CatType)
-                    {
-                        case "RdbMesh": // 1010001
-                            CatList = new ResourceTypeId[] { ResourceTypeId.RdbMesh };
-                            break;
-                        case "CatMesh": // 1010002
-                            CatList = new ResourceTypeId[] { ResourceTypeId.CatMesh };
-                            break;
-                        case "Anim": // 1010003
-                            CatList = new ResourceTypeId[] { ResourceTypeId.Anim };
-                            break;
-                        case "Texture": // 1010004
-                            CatList = new ResourceTypeId[] { ResourceTypeId.Texture };
-                            break;
-                        case "Icon": // 1010008
-                            CatList = new ResourceTypeId[] { ResourceTypeId.Icon };
-                            break;
-                        case "SkinTexture": // 1010011
-                            CatList = new ResourceTypeId[] { ResourceTypeId.SkinTexture };
-                            break;
-                        default: // 00000000
-                            Console.WriteLine("Error : wrong type ...");
-                            break;
-                    }                    
-                    foreach (var resource in CatList)
-                    {
-                        Console.WriteLine("Stored "+ CatType + " :");
-                        foreach (var rdbMeshKey in _rdbController.RecordTypeToId[(int)resource].Keys)
-                        {
-                            ICount = ICount + 1;
-                            if (!types[resource].TryGetValue(rdbMeshKey, out string name))
-                                Console.WriteLine(rdbMeshKey+" : Unamed");
-                            else
-                                Console.WriteLine(rdbMeshKey+" : "+name);
-                        }
-                    }
-                }
-                _rdbController.Dispose();
-                Console.WriteLine(ICount+" item(s) listed");
-                Console.Read();
+                    Console.WriteLine($"{key} : {name}");
             }
+
+        END:
+            _rdbController.Dispose();
+            Console.WriteLine("Done.");
+            Console.ReadLine();
         }
     }
 }
